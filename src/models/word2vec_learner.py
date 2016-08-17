54868b8781462aee14b1386076e25a9ad5b7b81f
Andrew <andrew@AndrewdeMacBook-Pro.local>
2016-08-17 17:47:00 +0800

    add class similarity & word2vec learner

---
+++ src/models/word2vec_learner.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# @file: word2vec_learner.py
+# @author: tangye <tangye@mgtv.com|tangyel@126.com>
+# @time: 16/8/17 下午5:45
+
--- src/similarities/similarity.py
+++ src/similarities/similarity.py
@@ -15,8 +15,31 @@
 # #  
 # ##
 
-class Similarity(object):
+import logging
+import itertools
 
+import numpy
+
+from gensim.models import Word2vec
+
+logger = logging.getLogger('similarities.similarity')
+
+
+class Similarity(object):
+	"""
+	Base class for similarities
+	"""
+	def __init__(self, num_features):
+		self.num_features = num_features
+
+	def compute(self, x, y):
+		raise NotImplementedError("not implement a abstract method")
+
+
+class DescSimilarity(Similarity):
+	"""
+	video description similarity using word2vec or lda
+	"""
 
 ## vim: set ts=2 sw=2: #
 

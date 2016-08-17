47b3a9da830c232adc1b4f46e69abec70ba6e350
ty02 <tangyel@126.com>
2016-08-17 16:46:58 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+#Content recommend part for video recommender system
+
+Extract video content infomation and compute structure similarity for them
+
+* **structural model merging different content parts **: title, tag, description, stars
+* **nlp techs** to compute similarity of each parts: tfidf, word2vec, ...
---
+++ src/similarities/similarity.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+############################################################################
+## 
+## Copyright (c) 2013 hunantv.com, Inc. All Rights Reserved
+## $Id: similarity.py,v 0.0 2016年08月17日 星期三 16时12分40秒  <> Exp $ 
+## 
+############################################################################
+#
+###
+# # @file   similarity.py 
+# # @author <tangye><<tangye@mgtv.com>>
+# # @date   2016年08月17日 星期三 16时12分40秒  
+# # @brief 
+# #  
+# ##
+
+class Similarity(object):
+
+
+## vim: set ts=2 sw=2: #
+
